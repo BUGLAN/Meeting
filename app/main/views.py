5d96207fd1f1788b390096d2e98cffe0ac821f7c
BUGLAN <1831353087@qq.com>
2018-01-21 10:45:20 +0800

    2018,1,21 commit

--- app/main/views.py
+++ app/main/views.py
@@ -3,8 +3,11 @@ from flask_restful import Resource, Api
 from .models import Meet
 from ext import db
 from flask import jsonify, request, make_response
+from .models import User
+from flask_cors import CORS
 
 main_api = Api(main_blueprint)
+CORS(main_blueprint, resources=r'/*')
 
 
 class MeetAdmin(Resource):
@@ -26,39 +29,79 @@ class MeetAdmin(Resource):
     """
     def post(self):
         if 'data' and 'operation' not in request.json:
-            return make_response(jsonify({"message": "格式错误"}), 400)
-        if 'name' and 'allow_people' and 'number' not in request.json['data']:
-            return make_response(jsonify({"message": "data 格式错误"}), 400)
-        if request.json['operation'] not in ['new', 'delete', 'change']:
-            return make_response(jsonify({"message": "operation 格式错误"}), 400)
-        if request.json['operation'] == 'new':
+            return make_response(jsonify({"error": "格式错误"}), 400)
+        elif 'name' and 'allow_people' and 'number' not in request.json['data']:
+            return make_response(jsonify({"error": "data 格式错误"}), 400)
+        elif request.json['operation'] not in ['new', 'delete', 'change']:
+            return make_response(jsonify({"error": "operation 格式错误"}), 400)
+        elif request.json['operation'] == 'new':
             meet = Meet(name=request.json['data']['name'], password='123456')
             db.session.add(meet)
             db.session.commit()
             return jsonify({"id": meet.id, "name": meet.name, "password": meet.password,
                             'create_time': meet.create_time})
-        if request.json['operation'] == 'delete':
+        elif request.json['operation'] == 'delete':
             return "delete Meeting"
-        if request.json['operation'] == 'change':
+        elif request.json['operation'] == 'change':
             return "change Meeting"
         else:
-            return "big error"
+            return make_response(jsonify({"error": "格式错误"}), 400)
 
 
 class Chat(Resource):
     """
     {
         "user" : {
-            name: "user.name",
+            "username": "user.username",
             "send_time": "datetime.datetime.now()"
         }
         "message": "send message"
-        "allow_people": "all", "just one", "part of people"
+        "allow_people": "all", "just one", "part of people" (可选)
         "meeting_room": "meet's name"
     }
     """
     def post(self):
-        pass
+        if 'user' and 'message' and 'meeting_room' not in request.json:
+            return make_response(jsonify({"error": "格式错误"}), 400)
+        # user = request.json['user']['name']
+        if request.json['user']['username'] and request.json['user']['send_time'] is None:
+            return make_response(jsonify({"error": "格式错误"}), 400)
+        user = User.query.filter(User.username == request.json['user']['username']).first()
+        if user:
+            return make_response(jsonify(
+                {
+                    "user": {
+                        "username": user.username,
+                        "send_time": request.json['user']['send_time']
+                    },
+                    "message": request.json['message'],
+                    "status": 200
+                }), 200)
+        else:
+            return make_response(jsonify({"error": "用户名错误"}), 400)
 
 
+class AjaxTest(Resource):
+    """
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'POST,GET,OPTIONS'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    """
+    def post(self):
+        if request.json:
+            message = {"message": "success", "status_code": 200}
+            response = make_response(jsonify(message))
+            response.headers['Access-Control-Allow-Origin'] = '*'
+            response.headers['Access-Control-Allow-Methods'] = 'OPTIONS,HEAD,GET,POST'
+            response.headers['Access-Control-Allow-Headers'] = 'x-requested-with'
+            return response
+        else:
+            message = {"message": "success", "status_code": 200}
+            res = make_response(jsonify(message))
+            return res
+
+
+# need more knowledge abort flask-restful
 main_api.add_resource(MeetAdmin, '/MeetAdmin')
+main_api.add_resource(Chat, '/ChatApi')
+main_api.add_resource(AjaxTest, '/AjaxTest')
